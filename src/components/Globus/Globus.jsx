99083253aec6d0afa8fed2a8824463285ec85103
KamIlI3 <122115129+KamIlI3@users.noreply.github.com>
2024-09-04 21:17:16 +0200

    Wiadomość commitu

--- src/components/Globus/Globus.jsx
+++ src/components/Globus/Globus.jsx
@@ -4,6 +4,20 @@ import styles from './Globus.module.css';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
+import earthmap from '../../assets/images/earthmap4k.jpg'
+import earthbump from '../../assets/images/earthbump4k.jpg'
+// import earthhiresclouds from '../../assets/images/earthhiresclouds4K.jpg'
+// import earthlights from '../../assets/images/earthlights4k.jpg'
+import earthspec from '../../assets/images/earthspec4k.jpg'
+
+import space_right from '../../assets/images/space_right.png'
+import space_left from '../../assets/images/space_left.png'
+import space_top from '../../assets/images/space_top.png'
+import space_bot from '../../assets/images/space_bot.png'
+import space_back from '../../assets/images/space_back.png'
+import space_front from '../../assets/images/space_front.png'
+
+
 class Globus extends Component{
 
 componentDidMount() {
@@ -24,10 +38,69 @@ createScene = () => {
   this.mount.appendChild(this.renderer.domElement);
   this.camera.position.z = 20;
 
-  const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-  const cube = new THREE.Mesh( geometry, material);
-  this.scene.add(cube);
+  this.addEarth();
+  this.addSkyBox();
+  this.addLight();
+}
+
+addEarth = () => {
+  
+  const earthMap = new THREE.TextureLoader().load( earthmap );
+  const earthBumpMap = new THREE.TextureLoader().load( earthbump );
+  const earthSpecMap = new THREE.TextureLoader().load( earthspec );
+
+  const earthGeometry = new THREE.SphereGeometry( 10, 32, 32 );
+  const earthMaterial = new THREE.MeshPhongMaterial({
+    map: earthMap,
+    bumpMap: earthBumpMap,
+    bumpScale: 0.1,
+    specularMap: earthSpecMap,
+    specular: new THREE.Color('gray')
+  });
+
+  this.earthSphere = new THREE.Mesh( earthGeometry, earthMaterial );
+  this.earthSphere.name = "EarthSphere";
+  this.scene.add( this.earthSphere );
+}
+
+addSkyBox = () => {
+
+  const skyBox = new THREE.CubeTextureLoader().load([
+
+    space_right,
+    space_left,
+    space_top,
+    space_bot,
+    space_front,
+    space_back,
+
+  ]);
+  this.scene.background = skyBox;
+}
+
+addLight = () => {
+  const sunLights =  [];
+    sunLights[0] = new THREE.DirectionalLight(0xffffff, 1);
+    sunLights[1] = new THREE.DirectionalLight(0xffffff, 1);
+    sunLights[2] = new THREE.DirectionalLight(0xffffff, 1);
+    sunLights[3] = new THREE.DirectionalLight(0xffffff, 1);
+    sunLights[4] = new THREE.DirectionalLight(0xffffff, 1);
+    sunLights[5] = new THREE.DirectionalLight(0xffffff, 1);
+
+    sunLights[0].position.set(0, 0, 1);
+    sunLights[1].position.set(0, 0, -1); 
+    sunLights[2].position.set(0, 1, 0);
+    sunLights[3].position.set(0, -1, 0); 
+    sunLights[4].position.set(1, 0, 0);
+    sunLights[5].position.set(-1, 0, 0); 
+
+    this.scene.add(sunLights[0]); 
+    this.scene.add(sunLights[1]);
+    this.scene.add(sunLights[2]); 
+    this.scene.add(sunLights[3]);
+    this.scene.add(sunLights[4]); 
+    this.scene.add(sunLights[5]);
+
 }
 
 startAnimation = () => {
