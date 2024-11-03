a4545338efc8ba5799617fd1b330da35a3434e87
KamIlI3 <122115129+KamIlI3@users.noreply.github.com>
2024-11-03 15:44:49 +0100

    ball grid added

--- src/components/Globus/helpers/earth.js
+++ src/components/Globus/helpers/earth.js
@@ -3,19 +3,26 @@ import * as THREE from 'three';
 import earthmap from '../../../assets/images/earthmap4k.jpg';
 import earthbump from '../../../assets/images/earthbump4k.jpg';
 import earthspec from '../../../assets/images/earthspec4k.jpg';
+import hexPattern from '../../../assets/images/balltexture.jpeg'; 
 
 export const createEarth = () => {
+
     const earthMap = new THREE.TextureLoader().load(earthmap);
     const earthBumpMap = new THREE.TextureLoader().load(earthbump);
     const earthSpecMap = new THREE.TextureLoader().load(earthspec);
+    const hexPatternMap = new THREE.TextureLoader().load(hexPattern); 
 
-    const earthGeometry = new THREE.SphereGeometry(10, 32, 32);
+ 
+    const earthGeometry = new THREE.SphereGeometry(10, 64, 64); 
+
     const earthMaterial = new THREE.MeshPhongMaterial({
-      map: earthMap,
-      bumpMap: earthBumpMap,
+      map: earthMap,                    
+      bumpMap: earthBumpMap,            
       bumpScale: 0.1,
-      specularMap: earthSpecMap,
+      specularMap: earthSpecMap,       
       specular: new THREE.Color('gray'),
+      normalMap: hexPatternMap,         
+      normalScale: new THREE.Vector2(2.0, 2.0), 
     });
 
     return new THREE.Mesh(earthGeometry, earthMaterial);
